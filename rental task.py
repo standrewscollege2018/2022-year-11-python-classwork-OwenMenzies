f9f38c807be37f3542029a19ce7585523a11d6ad
OwenMenzies <78829256+OwenMenzies@users.noreply.github.com>
2022-03-07 12:38:57 +1300

    set lists up
    lists now work

--- rental task.py
+++ rental task.py
@@ -1,1 +1,7 @@
 ''' rental cars '''
+cars = ["Toyota Corolla", "Honda CRV", "Suzuki Swift", "Suzuki Swift", "Nissan DC Ute", "Toyota Previa", "Toyota Hi Ace", "Toyota Hi Ac"]
+seats =["4","4","4","4","4","7","12","12"]
+print 
+car = len(cars)
+for i in range(1,car+1):
+    print (f"{i}. {cars[i-1]} - {seats}")
