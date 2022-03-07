685ce3a9b43de332d99c1ef10352fb11986fe2eb
OwenMenzies <78829256+OwenMenzies@users.noreply.github.com>
2022-03-08 10:31:20 +1300

    Update rental task.py
    simplified some code

--- rental task.py
+++ rental task.py
@@ -1,7 +1,16 @@
 ''' rental cars '''
-cars = ["Toyota Corolla", "Honda CRV", "Suzuki Swift", "Suzuki Swift", "Nissan DC Ute", "Toyota Previa", "Toyota Hi Ace", "Toyota Hi Ac"]
+cars = ["Toyota Corolla", "Honda CRV", "Suzuki Swift", "Suzuki Swift", "Nissan DC Ute", "Toyota Previa", "Toyota Hi Ace", "Toyota Hi Ace"]
 seats =["4","4","4","4","4","7","12","12"]
-print 
+availability = ["available", "available", "available", "available", "available", "available", "available", "available"]
+#set up lists
+#availability[1] = "unavailable" 
 car = len(cars)
 for i in range(1,car+1):
-    print (f"{i}. {cars[i-1]} - {seats}")
+    print (f"{i}. {cars[i-1]} - {seats[i-1]} seats {availability[i-1]}")
+print("What car would you like to resserve?(number)")
+choice = input()
+choice.lower() 
+for i in range(1,car+1):
+    
+    
+    
